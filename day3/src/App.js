89d46193ef5daffcfebd2c3491d796a228fa2d60
Deepak782001 <deepaksharma782001@gmail.com>
2022-12-08 13:27:12 +0530

    Navbars and login form

---
+++ day3/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './components/header';
+import LoginForm from './components/form';
+function App() {
+  return (
+    <div className='App'>
+      <Header />
+      <LoginForm />
+    </div>
+
+  );
+}
+
+export default App;
